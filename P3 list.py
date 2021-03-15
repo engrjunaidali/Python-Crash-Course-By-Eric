62ff8b70619f0927949775d90f086d20b2a61c8e
engrjunaidali <80405587+engrjunaidali@users.noreply.github.com>
2021-03-15 15:51:27 +0500

    Add files via upload

---
+++ P3 list.py
@@ -0,0 +1,16 @@
+bicycles = ["trek","cannodale","redline","specialized"]
+print(bicycles)
+
+#   Accessing a specific element from the list
+print(bicycles[0])
+
+#   string methods
+print(bicycles[0].title())
+
+#   Python has a special syntax for accessing the last element in a list
+print(bicycles[-1])
+
+#   Using Individual Values from a List
+message = "My bicycle was a " + bicycles[0].title() +"."
+print(message)
+
